aab959b4c7e5742d8d685219aa05169764565646
Manuel Condori <m4nu.condori007@gmail.com>
2024-04-04 23:51:26 -0500

    my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+my second project--my favorites blogs
---
+++ app.js
@@ -0,0 +1,41 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+
+mongoose.set('strictQuery', false)
+
+const mongoUrl = config.MONGODB_URI
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(mongoUrl)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+morgan.token('response-post', function (req) {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms -- :response-post'))
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
